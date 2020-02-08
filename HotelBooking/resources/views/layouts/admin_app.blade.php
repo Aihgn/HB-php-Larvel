e1cbd30938975b768776c85dc00962a12ff49237
Nghia <nghianht2205@gmail.com>
2020-02-08 20:58:37 +0700

    2820
    update front-end

--- HotelBooking/resources/views/layouts/admin_app.blade.php
+++ HotelBooking/resources/views/layouts/admin_app.blade.php
@@ -10,75 +10,70 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <!-- Styles -->
+    <link href="{{ asset('css/admin-bootstrap.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/admin-style.css') }}" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+
     <!-- Scripts -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>  
     <script src="{{ asset('js/jquery-3.4.1.min.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
     <script src="{{ asset('js/daterangepicker.js') }}"></script>
-
+    <script src="{{ asset('js/admin.js') }}"></script>
 
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <!-- <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script> -->
-
-
-    <!-- Styles -->
-    <link href="{{ asset('css/admin-style.css') }}" rel="stylesheet">
-    <!-- <link href="{{ asset('css/color/color.css') }}" rel="stylesheet"> -->
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
 
  
 </head>
 <body>
     <div class="wrap">                
-        <div id ="side-menu">
-            <a href="#"class="hide-menu-btn">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512" class="icon-2"><path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"/></svg>
-            </a>         
+        <div id ="side-menu">           
             <nav class="nav-menu">
-                <a href="{{route('admin')}}">
+                <a href="./profile.html" style="border-bottom: 1px solid #fbca04;margin-bottom: 1rem;border-top: 1px solid #fbca04;">
+                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="icon"><path d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"/></svg>
+                    <span class="span-side">Thông tin cá nhân</span>
+                </a>
+                <a href="{{route('admin')}}" class="isHover">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="icon"><path d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z"/></svg>
-                    <span>Home</span>
+                    <span class="span-side">Home</span>
                 </a>
-                <a href="{{route('book_off')}}">
+                <a href="{{route('book_off')}}" class="isHover">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="icon"><path d="M0 464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V192H0v272zm64-192c0-8.8 7.2-16 16-16h288c8.8 0 16 7.2 16 16v64c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16v-64zM400 64h-48V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H160V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H48C21.5 64 0 85.5 0 112v48h448v-48c0-26.5-21.5-48-48-48z"/></svg>
-                    <span>Booking Room</span>
+                    <span class="span-side">Booking Room</span>
                 </a>
-                <a href="{{route('all_res')}}">
+                <a href="{{route('all_res')}}" class="isHover">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="icon"><path d="M128 160h320v192H128V160zm400 96c0 26.51 21.49 48 48 48v96c0 26.51-21.49 48-48 48H48c-26.51 0-48-21.49-48-48v-96c26.51 0 48-21.49 48-48s-21.49-48-48-48v-96c0-26.51 21.49-48 48-48h480c26.51 0 48 21.49 48 48v96c-26.51 0-48 21.49-48 48zm-48-104c0-13.255-10.745-24-24-24H120c-13.255 0-24 10.745-24 24v208c0 13.255 10.745 24 24 24h336c13.255 0 24-10.745 24-24V152z"/></svg>
-                    <span>Reservation</span>
+                    <span class="span-side">Reservation</span>
                 </a>
-                {{-- <a href="{{route('check_in')}}">
+                <a href="{{route('check_in')}}" class="isHover">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="icon"><path d="M436 160H12c-6.627 0-12-5.373-12-12v-36c0-26.51 21.49-48 48-48h48V12c0-6.627 5.373-12 12-12h40c6.627 0 12 5.373 12 12v52h128V12c0-6.627 5.373-12 12-12h40c6.627 0 12 5.373 12 12v52h48c26.51 0 48 21.49 48 48v36c0 6.627-5.373 12-12 12zM12 192h424c6.627 0 12 5.373 12 12v260c0 26.51-21.49 48-48 48H48c-26.51 0-48-21.49-48-48V204c0-6.627 5.373-12 12-12zm333.296 95.947l-28.169-28.398c-4.667-4.705-12.265-4.736-16.97-.068L194.12 364.665l-45.98-46.352c-4.667-4.705-12.266-4.736-16.971-.068l-28.397 28.17c-4.705 4.667-4.736 12.265-.068 16.97l82.601 83.269c4.667 4.705 12.265 4.736 16.97.068l142.953-141.805c4.705-4.667 4.736-12.265.068-16.97z"/></svg>
-                    <span>Check-in</span>
-                </a> --}}
-                <a href="{{route('check_out')}}">
+                    <span class="span-side">Check-in</span>
+                </a>
+                <a href="{{route('check_out')}}" class="isHover">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="icon"><path d="M400 64h-48V12c0-6.627-5.373-12-12-12h-40c-6.627 0-12 5.373-12 12v52H160V12c0-6.627-5.373-12-12-12h-40c-6.627 0-12 5.373-12 12v52H48C21.49 64 0 85.49 0 112v352c0 26.51 21.49 48 48 48h352c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm-6 400H54a6 6 0 0 1-6-6V160h352v298a6 6 0 0 1-6 6zm-52.849-200.65L198.842 404.519c-4.705 4.667-12.303 4.637-16.971-.068l-75.091-75.699c-4.667-4.705-4.637-12.303.068-16.971l22.719-22.536c4.705-4.667 12.303-4.637 16.97.069l44.104 44.461 111.072-110.181c4.705-4.667 12.303-4.637 16.971.068l22.536 22.718c4.667 4.705 4.636 12.303-.069 16.97z"/></svg>
-                    <span>Check-out</span>
+                    <span class="span-side">Check-out</span>
                 </a>
-                <a href="{{route('manager_acc')}}">
+                <a href="{{route('manager_acc')}}" class="isHover">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" class="icon"><path d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm0 96c48.6 0 88 39.4 88 88s-39.4 88-88 88-88-39.4-88-88 39.4-88 88-88zm0 344c-58.7 0-111.3-26.6-146.5-68.2 18.8-35.4 55.6-59.8 98.5-59.8 2.4 0 4.8.4 7.1 1.1 13 4.2 26.6 6.9 40.9 6.9 14.3 0 28-2.7 40.9-6.9 2.3-.7 4.7-1.1 7.1-1.1 42.9 0 79.7 24.4 98.5 59.8C359.3 421.4 306.7 448 248 448z"/></svg>
-                    <span>Manager Account</span>
+                    <span class="span-side">Manager Account</span>
                 </a>
-                <a href="{{route('manager_room')}}">
+                <a href="{{route('manager_room')}}" class="isHover">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" class="icon"><path d="M624 448h-80V113.45C544 86.19 522.47 64 496 64H384v64h96v384h144c8.84 0 16-7.16 16-16v-32c0-8.84-7.16-16-16-16zM312.24 1.01l-192 49.74C105.99 54.44 96 67.7 96 82.92V448H16c-8.84 0-16 7.16-16 16v32c0 8.84 7.16 16 16 16h336V33.18c0-21.58-19.56-37.41-39.76-32.17zM264 288c-13.25 0-24-14.33-24-32s10.75-32 24-32 24 14.33 24 32-10.75 32-24 32z"/></svg>
-                    <span>Manager Room</span>
-                </a>  
+                    <span class="span-side">Manager Room</span>
+                </a>                   
             </nav>
         </div>
-        <header class="m-2">
+
+        <header class="container-fluid">
             <div class="row">               
-                <div class="ml-4 mr-4 show-menu-btn">
-                <a href="#" class="">
+           
+                <a href="#" class="show-menu-btn">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="icon-2"><path d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"/></svg>
-                </a>      
-                </div>
-                <div class="search-area row col-12 col-md-7">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="icon-header"><path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"/></svg>
-                    <input type="text" name="" value="" placeholder="Search" class="col-10 col-md-8">
-                </div>
+                </a> 
 
                 <div class="user-area row">
                     <a href="#" class="notification">
@@ -88,9 +83,8 @@
                     <div class="dropdown">
                         <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }}</a>
 
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuLink">
-                            <a class="dropdown-item" href="#">Profile</a>
-                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();"><i class="fas fa-sign-out-alt"></i>Logout
+                        <div class="dropdown-menu dropdown-menu-right mt-3" aria-labelledby="dropdownMenuLink">
+                            <a class="dropdown-item" href="{{route('logout')}}" onclick="event.preventDefault();document.getElementById('logout-form').submit();"><i class="fas fa-sign-out-alt"></i>Logout
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                     @csrf
                                 </form>
@@ -99,37 +93,22 @@
                     </div>            
                 </div>
             </div>
-            
         </header>
 
         <div class="content"> @yield('content') </div>
     </div>
-    
-<script type="text/javascript">
-    $(document).ready(function(){
-        var url = window.location.href;  
-        $(".nav-menu a").each(function(){
-            if (url == (this.href)) {
-                    $(this).closest("a").addClass("active");
+<script>
+    $(document).ready(function(){   
+        var mq = window.matchMedia( "(min-width: 992px)" );      
+         setInterval(function(){
+            if($(".wrap" ).hasClass("toggled") && mq.matches){
+                if($(".nav-menu").is(":hover")){
+                    $(".wrap").addClass("hover");
+                }else{
+                    $(".wrap").removeClass("hover");
                 }
-        });
-        $(".show-menu-btn").click(function(e){
-            e.preventDefault();
-            $("#side-menu").addClass("toggled");
-        });
-        $(".hide-menu-btn").click(function(e){
-            e.preventDefault();
-            $("#side-menu").removeClass("toggled");               
-        });
-        $(document).mouseup(function(e) 
-        {
-            var container = $("#side-menu");
-
-            if (!container.is(e.target) && container.has(e.target).length === 0) 
-            {
-                container.removeClass("toggled");
             }
-        });
+        }, 200);
     });
 </script>
 </body>
